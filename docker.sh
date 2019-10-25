11ba67c31070b54a94c5826528bbea8605fe049f
bdhwan <bdhwan@gmail.com>
2019-10-25 16:07:17 +0900

    log-bin-trust-function-creators=1 옵션 추가

--- docker.sh
+++ docker.sh
@@ -1,4 +1,4 @@
 
 #!/bin/bash
-docker build  --no-cache --tag bdhwan/mariadb:10.7.1 .
-docker push bdhwan/mariadb:10.7.1
+docker build  --no-cache --tag bdhwan/mariadb:10.7.2 .
+docker push bdhwan/mariadb:10.7.2
