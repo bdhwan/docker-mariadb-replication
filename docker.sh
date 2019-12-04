92867539c7c95fb1369c575f8faf417e8d963814
bdhwan <bdhwan@gmail.com>
2019-12-04 16:26:17 +0900

    fix timeout

--- docker.sh
+++ docker.sh
@@ -1,4 +1,4 @@
 
 #!/bin/bash
-docker build  --no-cache --tag bdhwan/mariadb:10.7.2 .
-docker push bdhwan/mariadb:10.7.2
+docker build  --no-cache --tag bdhwan/mariadb:10.7.3 .
+docker push bdhwan/mariadb:10.7.3
