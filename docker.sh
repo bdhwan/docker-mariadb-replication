52b8318ebc3bcdcea6da9dc1639a07d07702c55f
bdhwan <bdhwan@gmail.com>
2019-12-08 21:56:46 +0900

    increase innodb pool

--- docker.sh
+++ docker.sh
@@ -1,4 +1,4 @@
 
 #!/bin/bash
-docker build  --no-cache --tag bdhwan/mariadb:10.7.5 .
-docker push bdhwan/mariadb:10.7.5
+docker build  --no-cache --tag bdhwan/mariadb:10.7.6 .
+docker push bdhwan/mariadb:10.7.6
